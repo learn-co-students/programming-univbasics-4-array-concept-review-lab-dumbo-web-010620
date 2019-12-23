15c9a17dad0b9243a7330c3b6580e2b728b3d104
Kring Villanueva <ksvillanueva7@gmail.com>
2019-12-23 22:25:37 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,14 +1,13 @@
 def find_element_index(array, value_to_find)
   ### METHOD 1 ###
-  # array.length.times do |count|
-  #   if array[count] == value_to_find
-  #     return count
-  #     end
-  #     nil
-  #   end
+  array.length.times do |count|
+    if array[count] == value_to_find
+      return count
+      end  
+    end
 
   ### METHOD 2 ###
-  array.find_index(value_to_find)
+  # array.find_index(value_to_find)
 end
 
 def find_max_value(array)
